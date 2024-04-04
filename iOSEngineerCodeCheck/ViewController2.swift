716075d8ceb3a31d9d04a6e5f643313aceb61640
豊田 真吾 <toyodasinngo@gmail.com>
2024-04-04 10:42:50 +0900

    変数名を可読性の高いものに修正

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -10,38 +10,38 @@ import UIKit
 
 class ViewController2: UIViewController {
     
-    @IBOutlet weak var ImgView: UIImageView!
+    @IBOutlet weak var imgView: UIImageView!
     
-    @IBOutlet weak var TtlLbl: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var LangLbl: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
     
-    @IBOutlet weak var StrsLbl: UILabel!
-    @IBOutlet weak var WchsLbl: UILabel!
-    @IBOutlet weak var FrksLbl: UILabel!
-    @IBOutlet weak var IsssLbl: UILabel!
+    @IBOutlet weak var starsLbl: UILabel!
+    @IBOutlet weak var watchersLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var issuesLabel: UILabel!
     
     var vc1: ViewController!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let repo = vc1.repo[vc1.idx]
+        let repo = vc1.githubRepos[vc1.selectedIndex]
         
-        LangLbl.text = "Written in \(repo["language"] as? String ?? "")"
-        StrsLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
-        WchsLbl.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-        FrksLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-        IsssLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
-        getImage()
+        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        starsLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        fetchAndDisplayRepoImage()
         
     }
     
-    func getImage(){
+    func fetchAndDisplayRepoImage(){
         
-        let repo = vc1.repo[vc1.idx]
+        let repo = vc1.githubRepos[vc1.selectedIndex]
         
-        TtlLbl.text = repo["full_name"] as? String
+        titleLabel.text = repo["full_name"] as? String
         
         if let owner = repo["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
